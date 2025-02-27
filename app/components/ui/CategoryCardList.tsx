654d76d46710c0b28cdc49c946f00d731a748f3c
PioKl <kosiu342@gmail.com>
2025-02-27 23:17:31 +0100

    Add CategoryCardList component

---
+++ app/components/ui/CategoryCardList.tsx
@@ -0,0 +1,37 @@
+import data from "@/data/data.json";
+import Link from "next/link";
+import CategoryCard from "./CategoryCard";
+import styles from "@/styles/ui/categoryCardList.module.scss";
+
+interface CategoryCardListProps {
+  listType: "normal" | "menu";
+}
+
+export default function CategoryCardList({ listType }: CategoryCardListProps) {
+  const categoriesAndImage = data.map((item) => ({
+    category: item.category,
+    categoryThumbnail: item.categoryThumbnail,
+  }));
+  //Pojedyncze kategorie z obrazkami z thumbnail
+  const uniqueCategorysWithImage = categoriesAndImage.filter(
+    (item, index, self) =>
+      index === self.findIndex((x) => x.category === item.category)
+  );
+
+  return listType === "normal" ? (
+    <>
+      <span>Test</span>
+    </>
+  ) : (
+    <>
+      <ul className={`wrapper ${styles["card-list"]} ${styles[`--menu`]}`}>
+        <Link href="/" className={`nav-link ${styles["card-list__home-link"]}`}>
+          Home
+        </Link>
+        {uniqueCategorysWithImage.map((item, id) => (
+          <CategoryCard type="menu" key={id} categoriesWithImage={item} />
+        ))}
+      </ul>
+    </>
+  );
+}
