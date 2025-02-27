cc8dd49e78f20f3f0a8ad49a3412fc2633b4ad2f
PioKl <kosiu342@gmail.com>
2025-02-27 23:16:36 +0100

    Working Header in all resolutions

--- app/components/ui/Header.tsx
+++ app/components/ui/Header.tsx
@@ -1,23 +1,93 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import Link from "next/link";
 import Logo from "@/assets/shared/desktop/logo.svg";
 import BurgerMenu from "@/assets/shared/tablet/icon-hamburger.svg";
 import Cart from "@/assets/shared/desktop/icon-cart.svg";
 import styles from "@/styles/ui/header.module.scss";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+import CategoryCardList from "./CategoryCardList";
 
-export default function Header() {
+interface HeaderProps {
+  mainRef: React.RefObject<HTMLDivElement | null>;
+  footerRef: React.RefObject<HTMLDivElement | null>;
+}
+
+export default function Header({ mainRef, footerRef }: HeaderProps) {
+  const bodyRef = useRef<HTMLBodyElement>(null);
   const btnOpen = useRef<HTMLButtonElement>(null);
   const btnClose = useRef<HTMLButtonElement>(null);
   const navMenu = useRef<HTMLDivElement>(null);
 
+  //W celu uniknięcia błędu document is not defined gdy wcześniej tak było: const bodyRef = useRef<HTMLBodyElement>(document.body as HTMLBodyElement);
+  useEffect(() => {
+    bodyRef.current = document.body as HTMLBodyElement;
+  }, []);
+
   const handleBtnOpen = () => {
     btnOpen.current?.setAttribute("aria-expanded", "true");
+
+    //Odblokowanie możliwości interakcji (klawiatura i myszka) i widoczności dla accessibility
+    navMenu.current?.removeAttribute("inert");
+    navMenu.current?.removeAttribute("style");
+    //Zablokowanie możliwości interakcji
+    mainRef.current?.setAttribute("inert", "");
+    footerRef.current?.setAttribute("inert", "");
+
+    //Wyłączenie scrolla
+    if (bodyRef.current) {
+      disableBodyScroll(bodyRef.current);
+    }
+    //Ustaw focus na zamknięcie burgerMenu
     btnClose.current?.focus();
   };
   const handleBtnClose = () => {
     btnOpen.current?.setAttribute("aria-expanded", "false");
+
+    //Zablokowanie możliwości interakcji (klawiatura i myszka) i widoczności dla accessibility
+    navMenu.current?.setAttribute("inert", "");
+    //Odblokowanie możliwości interakcji
+    mainRef.current?.removeAttribute("inert");
+    footerRef.current?.removeAttribute("inert");
+
+    //Przywrócenie scrolla
+    if (bodyRef.current) {
+      enableBodyScroll(bodyRef.current);
+    }
+
+    //Ustaw focus na otwieranie burgerMenu
+    btnOpen.current?.focus();
+
+    //Żeby nie przesuwało się z dynamiczną zmianą rozdzielczości
+    setTimeout(() => {
+      if (navMenu.current) {
+        navMenu.current.style.transition = "none";
+      }
+    }, 500);
   };
+
+  useEffect(() => {
+    const media = window.matchMedia("(max-width: 48em)");
+    const setupNavMenu = () => {
+      if (media.matches && navMenu.current) {
+        //mobile
+        //Zablokowanie możliwości interakcji (klawiatura i myszka) i widoczności dla accessibility
+        navMenu.current.setAttribute("inert", "");
+
+        //Żeby nie przesuwało się z dynamiczną zmianą rozdzielczości
+        navMenu.current.style.transition = "none";
+      } else {
+        //desktop
+        handleBtnClose();
+        //Odblokowanie możliwości interakcji (klawiatura i myszka) i widoczności dla accessibility
+        navMenu.current?.removeAttribute("inert");
+      }
+    };
+    setupNavMenu();
+    media.addEventListener("change", setupNavMenu);
+    return () => media.removeEventListener("change", setupNavMenu);
+  });
+
   return (
     <header className={styles.header}>
       <div className={`wrapper ${styles.container}`}>
@@ -44,23 +114,21 @@ export default function Header() {
           <Link href="/">
             <Logo />
           </Link>
-          <button
-            className={`${styles["container__nav-button"]} ${styles["container__cart-button"]}`}
-          >
-            <Cart />
-          </button>
           <div
             className={styles["nav-menu"]}
             role="dialog"
             aria-labelledby="nav-label"
             ref={navMenu}
           >
-            <ul>
-              <li>Headphones</li>
-              <li>Speakers</li>
-              <li>Earphones</li>
-            </ul>
+            <div className={styles["nav-menu__item-list-container"]}>
+              <CategoryCardList listType="menu" />
+            </div>
           </div>
+          <button
+            className={`${styles["container__nav-button"]} ${styles["container__cart-button"]}`}
+          >
+            <Cart />
+          </button>
         </nav>
       </div>
     </header>
