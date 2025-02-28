75f4ca611b27d41eac335b8fb9bf5287288e19d8
PioKl <kosiu342@gmail.com>
2025-02-28 21:51:23 +0100

    Add Hero and Category Card List

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
+import styles from "./styles/home.module.scss";
+import Hero from "./components/Hero";
+import CategoryCardList from "./components/ui/CategoryCardList";
 
 export default function Home() {
   return (
-    <div>
-      <main></main>
-    </div>
+    <>
+      <Hero />
+      <div className={styles["category-card-list-home"]}>
+        <CategoryCardList listType="normal" />
+      </div>
+    </>
   );
 }
