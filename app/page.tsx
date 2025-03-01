362dec9dc73bd6a959180f10e4c8c34f993721c6
PioKl <kosiu342@gmail.com>
2025-03-01 22:06:21 +0100

    Add ProductBanner

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import styles from "./styles/home.module.scss";
 import Hero from "./components/Hero";
 import CategoryCardList from "./components/ui/CategoryCardList";
-
+import ProductBanner from "./components/ui/ProductBanner";
 export default function Home() {
   return (
     <>
@@ -10,6 +10,11 @@ export default function Home() {
       <div className={styles["category-card-list-home"]}>
         <CategoryCardList listType="normal" />
       </div>
+      <section className={styles["product-banner-list"]}>
+        <ProductBanner type="one" productName="ZX9 Speaker" />
+        <ProductBanner type="two" productName="ZX7 Speaker" />
+        <ProductBanner type="three" productName="YX1 Earphones" />
+      </section>
     </>
   );
 }
