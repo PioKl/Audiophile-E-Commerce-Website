31e0257115ece67fcb526b95e8de5d6b0fa50537
PioKl <kosiu342@gmail.com>
2025-03-03 19:10:34 +0100

    Add FeatureBlock in Home

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./styles/home.module.scss";
 import Hero from "./components/Hero";
 import CategoryCardList from "./components/ui/CategoryCardList";
 import ProductBanner from "./components/ui/ProductBanner";
+import FeatureBlock from "./components/ui/FeatureBlock";
 export default function Home() {
   return (
     <>
@@ -10,10 +11,20 @@ export default function Home() {
       <div className={styles["category-card-list-home"]}>
         <CategoryCardList listType="normal" />
       </div>
-      <section className={styles["product-banner-list"]}>
+      <section className={styles["product-banner-list-section"]}>
         <ProductBanner type="one" productName="ZX9 Speaker" />
         <ProductBanner type="two" productName="ZX7 Speaker" />
         <ProductBanner type="three" productName="YX1 Earphones" />
+      </section>
+      <section className={styles["feature-section"]}>
+        <FeatureBlock
+          heading="Bringing you the best audio gear"
+          headingWordToColor={4}
+          text="Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment."
+          imageMobile="/assets/shared/mobile/image-best-gear.jpg"
+          imageTablet="/assets/shared/tablet/image-best-gear.jpg"
+          imageDesktop="/assets/shared/desktop/image-best-gear.jpg"
+        />
       </section>
     </>
   );
