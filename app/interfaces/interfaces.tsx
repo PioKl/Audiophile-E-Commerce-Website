e4333182cad266c2964ba114985d475beb3a93ae
PioKl <kosiu342@gmail.com>
2025-03-05 19:28:39 +0100

    New interfaces

--- app/interfaces/interfaces.tsx
+++ app/interfaces/interfaces.tsx
@@ -12,3 +12,28 @@ export interface CategoryProduct {
   category: string;
   new: boolean;
 }
+
+export interface ProductDetails {
+  image: ProductImage;
+  new: boolean;
+  name: string;
+  description: string;
+  price: number;
+  others: Others[];
+}
+
+export interface Others {
+  image: ProductImage;
+  slug: string;
+  name: string;
+}
+
+export interface Includes {
+  quantity: number;
+  item: string;
+}
+
+export interface ProductFeatures {
+  features: string;
+  includes: Includes[];
+}
