d9bf59cec21609c638870d0260a4ce9c648607ad
PioKl <kosiu342@gmail.com>
2025-03-06 18:22:22 +0100

    Add new components

--- app/(shop)/[category]/[slug]/page.tsx
+++ app/(shop)/[category]/[slug]/page.tsx
@@ -2,7 +2,12 @@ import data from "@/data/data.json";
 import { ProductParams } from "@/types/types";
 import { notFound } from "next/navigation";
 import ProductDetailsCard from "@/components/ui/ProductDetailsCard";
+import ProductFeatures from "@/components/ui/ProductFeatures";
+import ProductGallery from "@/components/ui/ProductGallery";
+import CategoryCardList from "@/components/ui/CategoryCardList";
+import FeatureBlock from "@/components/ui/FeatureBlock";
 import styles from "@/styles/productDetailsPage.module.scss";
+
 export async function generateStaticParams() {
   const categories = data;
 
@@ -34,24 +39,43 @@ export default async function ProductDetails({
   return (
     <div className="wrapper">
       <span>{resolvedParams.slug}</span>
+
       <ProductDetailsCard
         type="details"
         productDetailsData={categoryWithSlugData}
       />
-      <section className={styles["product-details-also-like-section"]}>
-        <h3 className={styles["product-details-also-like-section__heading"]}>
+      <ProductFeatures
+        features={categoryWithSlugData.features}
+        includes={categoryWithSlugData.includes}
+      />
+      <ProductGallery gallery={categoryWithSlugData.gallery} />
+      <div className={styles["product-details-also-like"]}>
+        <h3 className={styles["product-details-also-like__heading"]}>
           You May Also Like
         </h3>
         <ul
-          className={
-            styles["product-details-also-like-section__also-like-cards-list"]
-          }
+          className={styles["product-details-also-like__also-like-cards-list"]}
         >
           <ProductDetailsCard
             type="also-like"
             productDetailsData={categoryWithSlugData}
           />
         </ul>
+      </div>
+
+      <section className="category-card-list-pages">
+        <CategoryCardList listType="normal" />
+      </section>
+
+      <section className="feature-section">
+        <FeatureBlock
+          heading="Bringing you the best audio gear"
+          headingWordToColor={4}
+          text="Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment."
+          imageMobile="/assets/shared/mobile/image-best-gear.jpg"
+          imageTablet="/assets/shared/tablet/image-best-gear.jpg"
+          imageDesktop="/assets/shared/desktop/image-best-gear.jpg"
+        />
       </section>
     </div>
   );
