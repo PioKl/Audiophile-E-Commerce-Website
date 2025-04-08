2c06f75265dbe39bc8ae6784b375068d7b7cadf8
PioKl <kosiu342@gmail.com>
2025-04-08 20:38:43 +0200

    Loader if user is not logged in then redirect to previous website (code in AuthContext)

--- app/(shop)/checkout/page.tsx
+++ app/(shop)/checkout/page.tsx
@@ -1,14 +1,30 @@
+"use client";
+import { useContext } from "react";
 import styles from "@/styles/checkoutPage.module.scss";
 import GoBackButton from "@/components/ui/GoBackButton";
 import FormCheckout from "@/components/checkout/FormCheckout";
+import AuthContext from "@/contexts/AuthContext";
+import Loader from "@/components/ui/Loader";
 
 export default function Checkout() {
+  const { isUserLoggedIn } = useContext(AuthContext);
+
   return (
-    <section className={`${styles["checkout-section"]}`}>
-      <div className="wrapper">
-        <GoBackButton />
-      </div>
-      <FormCheckout />
+    <section
+      className={`${styles["checkout-section"]} ${
+        !isUserLoggedIn && styles[`--no-auth`]
+      }`}
+    >
+      {isUserLoggedIn ? (
+        <>
+          <div className="wrapper">
+            <GoBackButton />
+          </div>
+          <FormCheckout />
+        </>
+      ) : (
+        <Loader aria-label="Checking if user is logged in" />
+      )}
     </section>
   );
 }
