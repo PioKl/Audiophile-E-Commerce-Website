b07b3b2bb62ea440dcb9e29963ce3eaec641992c
PioKl <kosiu342@gmail.com>
2025-04-26 17:36:32 +0200

    Add CartProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Manrope } from "next/font/google";
 import "./styles/base/globals.scss";
 import ClientLayout from "./components/ClientLayout";
 import { AuthProvider } from "./contexts/AuthContext";
+import { CartProvider } from "./contexts/CartContext";
 import { ToastContainer } from "react-toastify";
 
 const manrope = Manrope({
@@ -23,14 +24,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-        <body
-          className={`${manrope.className}`}
-          suppressHydrationWarning={true}
-        >
-          <ToastContainer />
-          <div id="modal-hook"></div> {/* dla sposobu bez mui modal */}
-          <ClientLayout>{children}</ClientLayout>
-        </body>
+        <CartProvider>
+          <body
+            className={`${manrope.className}`}
+            suppressHydrationWarning={true}
+          >
+            <ToastContainer />
+            <div id="modal-hook"></div> {/* dla sposobu bez mui modal */}
+            <ClientLayout>{children}</ClientLayout>
+          </body>
+        </CartProvider>
       </AuthProvider>
     </html>
   );
