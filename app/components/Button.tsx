5a8229c74408ce952269ae5a8cd46ade7e49f25b
PioKl <kosiu342@gmail.com>
2025-05-05 15:01:02 +0200

    Add loader to button with interface

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import styles from "../styles/button.module.scss";
+import stylesLoader from "../styles/ui/loader.module.scss";
+import Loader from "./ui/Loader";
 
 interface NormalButtonProps {
   buttonType: "one" | "two" | "three" | "four";
@@ -8,19 +10,34 @@ interface NormalButtonProps {
   isALink: boolean;
   onClick?: () => void;
   disabled?: boolean;
+  isButtonWithLoader?: boolean;
 }
 
 interface LinkButtonProps extends NormalButtonProps {
   isALink: true;
   link: string;
+  isButtonWithLoader?: never; // Loader nie dotyczy linków
+  loader?: never; // Loader nie dotyczy linków
 }
 
-interface WithoutLinkButtonProps extends NormalButtonProps {
+interface WithoutLinkWithLoaderButtonProps extends NormalButtonProps {
   isALink: false;
   link?: never;
+  isButtonWithLoader: true;
+  loader: boolean;
 }
 
-type ButtonProps = LinkButtonProps | WithoutLinkButtonProps; //z linkiem lub bez może być, jak będzie isALink to wyskoczy, że poda link, inaczej nic się nie pojawi jeśli isALink jest false
+interface WithoutLinkWithoutLoaderButtonProps extends NormalButtonProps {
+  isALink: false;
+  link?: never;
+  isButtonWithLoader?: false;
+  loader?: never;
+}
+
+type ButtonProps =
+  | LinkButtonProps
+  | WithoutLinkWithLoaderButtonProps
+  | WithoutLinkWithoutLoaderButtonProps; //z linkiem lub bez może być, jak będzie isALink to wyskoczy, że poda link, inaczej nic się nie pojawi jeśli isALink jest false, także może być z loaderem lub bez, jak będzie isButtonWithLoader na true to wyskoczy, że trzaba przekazać loadera (będzie to jego stan)
 
 const Button: React.FC<ButtonProps> = ({
   buttonType,
@@ -30,10 +47,17 @@ const Button: React.FC<ButtonProps> = ({
   link,
   onClick,
   disabled,
+  isButtonWithLoader,
+  loader,
 }) => {
   const buttonClass = `${styles["btn"]} ${styles[`--${buttonType}`]} ${
     isInNav && styles["--nav"]
-  } ${disabled && styles["--disabled"]}`;
+  } ${disabled && styles["--disabled"]} ${
+    isButtonWithLoader && styles["--with-loader"]
+  }`;
+  const loaderClass = `${
+    isButtonWithLoader && stylesLoader["loader-in-button"]
+  }`;
   return isALink ? (
     <Link
       href={link as string}
@@ -45,9 +69,11 @@ const Button: React.FC<ButtonProps> = ({
       {text}
     </Link>
   ) : (
-    <button className={buttonClass} onClick={onClick}>
-      {text}
-    </button>
+    <>
+      <button className={buttonClass} onClick={onClick}>
+        {loader ? <Loader className={loaderClass} /> : text}
+      </button>
+    </>
   );
 };
 
